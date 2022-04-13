8b6b6e053a6fec5f77a35fa1746503ff2f943386
menukaghalan <59702166+menukaghalan@users.noreply.github.com>
2022-04-13 09:53:50 -0400

    Update test_line.py

--- test_line.py
+++ test_line.py
@@ -18,21 +18,4 @@ class TestLine(unittest.TestCase):
         result = Point.distance(5,8,3,4)
         self.assertNotEqual(result, "hello")
 
-# def test_init_():
-#     P = Point(5,8,3,4)
-#     assert isinstance(P, Point)
-
-# def test_get_x1():
-#     x1 = line_obj.x1
-#     x1_2 = line_obj.get_x1()
-
-#     assert x1 == x1_2
-
-# def test_set_x1():
-#     line_obj.set_x1(5)
-#     assert line_obj.x1 == 5
-
-#     # line_obj.set_x1("hehe")
-#     # assert line_obj.x1 == 5
-
 
