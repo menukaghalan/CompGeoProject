66351443b7ecc613e6b2a0e586696b4b1bccc172
menukaghalan <59702166+menukaghalan@users.noreply.github.com>
2022-04-13 03:40:06 -0400

    Created test_line
    Created three different test cases

---
+++ test_line.py
@@ -0,0 +1,38 @@
+import unittest
+from line import Point
+
+# line_obj = Point(5, 8, 3, 4)
+# print("Slope of the points is = ", line_obj.slope())
+# print("Distance between the two points is = ", line_obj.distance())
+
+class TestLine(unittest.TestCase):
+    def test_slope(self):
+        result = Point.slope(5,8,3,4)
+        self.assertEqual(result, 2)
+
+    def test_distance(self):
+        result = Point.distance(5,8,3,4)
+        self.assertEqual(result, 4.47213595499958)
+
+    def test_distance(self):
+        result = Point.distance(5,8,3,4)
+        self.assertEqual(result, "hello")
+
+# def test_init_():
+#     P = Point(5,8,3,4)
+#     assert isinstance(P, Point)
+
+# def test_get_x1():
+#     x1 = line_obj.x1
+#     x1_2 = line_obj.get_x1()
+
+#     assert x1 == x1_2
+
+# def test_set_x1():
+#     line_obj.set_x1(5)
+#     assert line_obj.x1 == 5
+
+#     # line_obj.set_x1("hehe")
+#     # assert line_obj.x1 == 5
+
+
